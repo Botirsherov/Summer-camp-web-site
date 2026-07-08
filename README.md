10d5e25566b5328b5f9e76f079c26dd9fb694f0a
Abdulatif <abdulatifbotirsherov1485@gmail.com>
2026-07-08 14:09:29 +0500

    Js added

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # My personal site for Summer camp
 
 site is under development
+
+## Files
+
+- `index.html` -  this is where everything starts.
+- `style.css` - beauty of my site.
+- `script.js` - annoying alerts live here.
---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', function () {
+  alert('🏁 Welcome to the Summer Camp Website! 🏎️');
+  alert('This is me Abdulatif');
+  alert('You visited my site');
+  alert('I know this alert is annoying');
+  alert('But it is fun 🤣')
+});
+
+console.log('Here Console')
+console.log('You can not see this message until inspectign the page')
